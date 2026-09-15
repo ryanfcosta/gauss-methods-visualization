cf5f38ee69e706a74830eb95a3ed2ce496db1383
Ryan Costa <89921782+ryanfcosta@users.noreply.github.com>
2026-09-15 12:18:15 -0300

    feat methods buttons ui and reset matrix function

--- main.cpp
+++ main.cpp
@@ -17,10 +17,36 @@
 #include <QString>
 #include <QFile>
 #include <QTextStream>
+#include <QPushButton>
 
-#define NUM 3
+#define NUM 4
 
 using namespace std;
+
+void resetMatrix(double**a, const double a_temp[NUM][NUM], vector <double> vars, double*b, const double b_temp[NUM], QGridLayout *matrix_layout, QLabel* nums_matrix[NUM][NUM + 1]){
+    vars.assign(NUM, 0.0);
+    for (int i = 0; i < NUM; i++) {
+        b[i] = b_temp[i];
+
+        nums_matrix[i][NUM] = new QLabel();
+
+        QString result_b = QString("%1").arg(b[i], 0, 'f', 4);
+        nums_matrix[i][NUM] -> setText(result_b);
+        nums_matrix[i][NUM]->setStyleSheet("border-left: 3px solid #FF5C00;");
+        matrix_layout->addWidget(nums_matrix[i][NUM], i, NUM);
+        a[i] = new double[NUM];
+        for (int j = 0; j < NUM; j++) {
+            a[i][j] = a_temp[i][j];
+
+            nums_matrix[i][j] = new QLabel();
+
+            QString result_text = QString("%1").arg(a[i][j], 0, 'f', 4);
+            nums_matrix[i][j] -> setText(result_text);
+                
+            matrix_layout->addWidget(nums_matrix[i][j], i, j);
+        }
+    }
+}
 
 void exibeMatriz(double **a ,double *b){
     for(int i = 0; i < NUM ; i++){
@@ -77,40 +103,23 @@ int main(int argc, char* argv[]){
     cout << fixed << setprecision(9);
 
     double a_temp[NUM][NUM] = {
-        {-0.0000,1.0000,0.8415},
-        {3,-1.0000,0.0000},
-        {14.7781, 0.0000,-1.0000}
+        {-18, 3, 4, -5},
+        {1, -6, 0, 3},
+        {1, 4, -8, -1},
+        {-2, 3, -4, -10 }
     };
 
     double b_temp[NUM] {
-        0.5403, 3, 15.3890
+        0, 10, 5, -3
     };
 
     double **a = new double*[NUM];
     double b[NUM];
-    for (int i = 0; i < NUM; i++) {
-        b[i] = b_temp[i];
-
-        nums_matrix[i][NUM] = new QLabel();
-
-        QString result_b = QString("%1").arg(b[i], 0, 'f', 4);
-        nums_matrix[i][NUM] -> setText(result_b);
-        nums_matrix[i][NUM]->setStyleSheet("border-left: 3px solid #FF5C00;");
-        matrix_layout->addWidget(nums_matrix[i][NUM], i, NUM);
-        a[i] = new double[NUM];
-        for (int j = 0; j < NUM; j++) {
-            a[i][j] = a_temp[i][j];
-
-            nums_matrix[i][j] = new QLabel();
-
-            QString result_text = QString("%1").arg(a[i][j], 0, 'f', 4);
-            nums_matrix[i][j] -> setText(result_text);
-            
-            matrix_layout->addWidget(nums_matrix[i][j], i, j);
-        }
-    }
     vector <double> vars(NUM, 0.0);
+
+    resetMatrix(a, a_temp, vars, b, b_temp, matrix_layout, nums_matrix);
     QLabel * var_labels[NUM];
+
     for(int i = 0; i < NUM; i++){
         var_labels[i] = new QLabel();
 
@@ -134,6 +143,8 @@ int main(int argc, char* argv[]){
         var_labels[i]->setText(result_text);
     }
 
+    resetMatrix(a, a_temp, vars, b, b_temp, matrix_layout, nums_matrix);
+    
     for(auto const x : vars){
         cout << x << " ";
     } cout << endl;
@@ -154,7 +165,7 @@ int main(int argc, char* argv[]){
     GJacobi::solve(a_new,vars,b,NUM);
 
     for(auto const x : vars){
-        cout << x << " ";
+        cout << x << " "; vars.assign(NUM, 0.0);
     }cout << endl;
 
     vars.assign(NUM, 0.0);
@@ -166,7 +177,43 @@ int main(int argc, char* argv[]){
 
     for (int i = 0; i < NUM; i++) delete[] a_new[i];
     delete[] a_new;
+        
+    QPushButton *button_elimin = new QPushButton("Eliminação Gaussiana");
+    methods_container->addWidget(button_elimin);
 
+    QObject::connect(button_elimin, &QPushButton::clicked, [&a,a_temp, &vars,&b, b_temp, var_labels, matrix_layout, &nums_matrix](){
+        resetMatrix(a, a_temp, vars, b, b_temp, matrix_layout, nums_matrix);
+        Elimination::solve(a,vars,b, NUM);
+        for (int i = 0; i < NUM; i++) {
+            QString result_text = QString("x%1 = %2").arg(i + 1).arg(vars[i], 0, 'f', 4);
+            var_labels[i]->setText(result_text);
+        }
+    });
+
+    QPushButton *button_jacobi = new QPushButton("Gauss Jacobi");
+    methods_container->addWidget(button_jacobi);
+    QObject::connect(button_jacobi, &QPushButton::clicked, [&a,a_temp, &vars,&b, b_temp, var_labels, matrix_layout, &nums_matrix](){
+        resetMatrix(a, a_temp, vars, b, b_temp, matrix_layout, nums_matrix);
+        GJacobi::solve(a,vars,b, NUM);
+        for (int i = 0; i < NUM; i++) {
+            QString result_text = QString("x%1 = %2").arg(i + 1).arg(vars[i], 0, 'f', 4);
+            var_labels[i]->setText(result_text);
+        }
+    });
+
+
+    QPushButton *button_seidel = new QPushButton("Gauss Seidel");
+    methods_container->addWidget(button_seidel);
+    QObject::connect(button_seidel, &QPushButton::clicked, [&a,a_temp, &vars,&b, b_temp, var_labels, matrix_layout, &nums_matrix](){
+        resetMatrix(a, a_temp, vars, b, b_temp, matrix_layout, nums_matrix);
+        GSeidel::solve(a,vars,b, NUM);
+        for (int i = 0; i < NUM; i++) {
+            QString result_text = QString("x%1 = %2").arg(i + 1).arg(vars[i], 0, 'f', 4);
+            var_labels[i]->setText(result_text);
+        }
+    });
+
+    buttons_layout->addItem(methods_container);
     janela.show();
     return app.exec();
 }
